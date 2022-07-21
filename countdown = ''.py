ad586efccefbd024379daeed4d3f55390c5d83dc
zasutsky <zasutsky_aleksey@mail.ru>
2022-07-21 15:31:28 +0500

    Initial commit

---
+++ countdown = ''.py
@@ -0,0 +1,12 @@
+for current_hour in range(0, 24):
+    print("На часах " + str(current_hour) + ":00.")
+    if current_hour < 6:
+        print('Доброй ночи!')
+    elif current_hour < 12:
+        print('Доброе утро!')
+    elif current_hour < 18:
+        print('Добрый день!')
+    elif current_hour <  23:
+        print('Добрый вечер!')        
+    else:
+        print('Доброй ночи!')
